08813329a0bdd0468d6de2566ed63a5fa0da772d
AlexDolz <aleksejdolznikov3@gmail.com>
2023-02-13 14:32:01 +0100

    practice

---
+++ Lesson_20_13_02_DOM_fetch_tasks/lesson_20_13_02.js
@@ -0,0 +1,83 @@
+// *********************** Task 1 ************************************
+// 1. Написать функцию, которая принимает id и при вызове которой отправляется fetch-запрос по адресу https://jsonplaceholder.typicode.com/users/id
+
+// 2. Вывести в консоль то, что придет по запросу
+
+// function getUsers(id) {
+//   fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+//     .then(res => res.json())
+//     .then(json => render(json)); // returns Object {}
+// }
+
+// getUsers(1);
+
+// ************************ Task 2 ***********************************
+// 3. Через дом наполнить users_container данными, полученными из ответа на запрос - отобразить в браузере имя и телефон пользователя
+
+// let usersContainer = document.querySelector('.users_container');
+// function render({ name, phone }) {
+//   usersContainer.innerHTML = '';
+//   const userData = document.createElement('p');
+//   userData.innerText = `${name} ${phone}`;
+
+//   usersContainer.append(userData);
+// }
+
+// *************************** Task 3 *********************************
+
+// 4. На кпоки повесить слушатели событий. при клике на левую мы отправляем запрос на id текущего юзера - 1, при клике на праву - на id текущего юзера + 1
+// let amount = 1;
+
+// const [buttonLeft, buttonRight] = document.querySelectorAll('button');
+
+// buttonLeft.addEventListener('click', () => {
+//   if (amount === 1) {
+//     amount = 10;
+//   }
+//   getUsers(--amount);
+// });
+// buttonRight.addEventListener('click', () => {
+//   if (amount === 10) {
+//     amount = 1;
+//   }
+//   getUsers(++amount);
+// });
+
+// **************************** Task 4 ********************************
+// 1. Написать функцию, при вызове которой отправляется fetch-запрос по адресу https://jsonplaceholder.typicode.com/todos
+// 2. Вывести в консоль то, что придет по запросу
+
+function toDo() {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json())
+    .then(json => taskRender(json)); // returns array []
+}
+
+toDo();
+
+// 3. Наполнить контейнер карточками с двумя параграфами (task и status). В status выводить текст 'done', если задача выполнена, и 'not done' - если не выполнена
+
+const todosContainer = document.querySelector('.todos_container');
+
+function taskRender(tasks) {
+  const taskCards = tasks.map(elem => {
+    const card = document.createElement('div');
+    const taskElem = document.createElement('p');
+    const statusELem = document.createElement('p');
+
+    card.classList.add('card');
+
+    taskElem.innerText = `Task: ${elem.title}`;
+    if (elem.completed == false) {
+      statusELem.innerText = `Status: ${'not done'}`;
+      card.style.backgroundColor = 'azure';
+    } else if (elem.completed == true) {
+      statusELem.innerText = 'done';
+      card.style.backgroundColor = 'lightgreen';
+    }
+
+    card.append(taskElem, statusELem);
+    return card;
+  });
+  todosContainer.append(...taskCards);
+}
